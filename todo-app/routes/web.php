24b020da5baaac5b3a0386bc4c706e58262e9a51
fathimasamee <fathimasameera0610@gmail.com>
2025-03-23 14:00:54 +0530

    update web.php

--- todo-app/routes/web.php
+++ todo-app/routes/web.php
@@ -1,13 +1,16 @@
 <?php
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return redirect('/tasks');
 });
 
-Route::get('/tasks', 'App\Http\Controllers\TaskController@index')->name('tasks.index');
-Route::post('/tasks', 'App\Http\Controllers\TaskController@store')->name('tasks.store');
-Route::put('/tasks/{task}', 'App\Http\Controllers\TaskController@update')->name('tasks.update');
-Route::delete('/tasks/{task}', 'App\Http\Controllers\TaskController@destroy')->name('tasks.destroy');
-
-Route::post('/tasks/{task}/toggle', 'App\Http\Controllers\TaskController@toggle')->name('tasks.toggle');
-Route::post('/tasks/clear-completed', 'App\Http\Controllers\TaskController@clearCompleted')->name('tasks.clear-completed');
-
+Route::controller(TaskController::class)->group(function () {
+    Route::get('/tasks', 'index')->name('tasks.index');
+    Route::post('/tasks', 'store')->name('tasks.store');
+    Route::put('/tasks/{task}', 'update')->name('tasks.update');
+    Route::delete('/tasks/{task}', 'destroy')->name('tasks.destroy');
+    Route::post('/tasks/{task}/toggle', 'toggle')->name('tasks.toggle');
+    Route::post('/tasks/clear-completed', 'clearCompleted')->name('tasks.clear-completed');
+});
